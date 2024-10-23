580a6a2c8b311f8cabdce657e17bede7fdba0c5f
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2024-10-23 15:35:13 -0400

    get user choice from button

--- game.js
+++ game.js
@@ -6,15 +6,28 @@ function getComputerChoice() {
     } else if (computerChoice < 2/3) {
         return "paper";
     } else {
-        return "scissors";
+        return "scisItssors";
     }
 }
 
-// collect choice from the user
-function getHumanChoice() {
-    let humanChoice = prompt("Rock Paper or Scissors?");
-    return humanChoice.toLowerCase();
-}
+// get user choice from button
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        let userChoice = null
+        
+        if (e.target.textContent === 'Rock') {
+            userChoice = 'rock'
+        } else if (e.target.textContent === 'Paper') {
+            userChoice = 'paper'
+        } else {
+            userChoice = 'scissors'
+        }
+
+        console.log(userChoice)
+    })
+})
 
 // set human and computer score variables
 let humanScore = 0;
